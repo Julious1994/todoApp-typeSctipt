a381af3db53b8696246f10fb0552ef446e2aa4d8
Amit Patel <mtpatel557@yahoo.com>
2018-12-21 12:47:11 +0530

    change todo text to object

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,21 +1,28 @@
 import * as React from "react";
-import { HandleOnChangeInterface } from './commonInterface';
+import HandleOnChangeInterface from './commonInterface';
 
 interface formProps {
-  text?: string;
+  todo: {
+    text: string,
+    selected?: boolean,
+  };
   onClick(): void;
   onChange(e: HandleOnChangeInterface): void;
 }
 
-const Form = (props: formProps) => (
-  <div>
-    <input value={props.text} type="text" onChange={props.onChange} />
-    <button onClick={props.onClick}>Add todo</button>
-  </div>
-);
+const Form = (props: formProps) => {
+  return (
+    <div>
+      <input value={props.todo.text} type="text" onChange={props.onChange} />
+      <button onClick={props.onClick}>Add todo</button>
+    </div>
+  )
+};
 
 Form.defaultProps = {
-  text: ""
+  todo: {
+    text: '',
+  }
 };
 
 export default Form;
--- src/__tests__/form.test.tsx
+++ src/__tests__/form.test.tsx
@@ -1,29 +1,31 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
-import Form from './../Form';
+import Form from '../Form';
 
 let wrapper: any;
-let onChangeMock = jest.fn();
-let onClickMock = jest.fn();
+const onChangeMock = jest.fn();
+const onClickMock = jest.fn();
 
 beforeEach(() => {
-    const props = {
-        onChange: onChangeMock,
-        onClick: onClickMock,
-        text: '',
-    };
-    wrapper = shallow(<Form {...props} />)
+  const props = {
+    onChange: onChangeMock,
+    onClick: onClickMock,
+    todo: {
+      text: '',
+    },
+  };
+  wrapper = shallow(<Form {...props} />);
 });
 
 it('should render form view', () => {
-    const input = wrapper.find('input');
-    const button = wrapper.find('button');
-    expect(input.length).toBe(1);
-    expect(button.length).toBe(1);
-    expect(wrapper).toMatchSnapshot();
+  const input = wrapper.find('input');
+  const button = wrapper.find('button');
+  expect(input.length).toBe(1);
+  expect(button.length).toBe(1);
+  expect(wrapper).toMatchSnapshot();
 });
 
 test('button title', () => {
-    const button = wrapper.find('button').at(0);
-    expect(button.text()).toBe('Add todo');
+  const button = wrapper.find('button').at(0);
+  expect(button.text()).toBe('Add todo');
 });
